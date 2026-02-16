151ee4baffdf00b20aa3920ded4f07b0034a3f60
meqvabidzegio <meqvabidzegio@gmail.com>
2026-02-16 17:53:44 +0100

    second commit

---
+++ Level 4/Homework/lesson 4.py
@@ -0,0 +1,65 @@
+#დავალება 2
+#price = "23"
+#1name = "gio"
+#num1 = "gio" - 23
+
+#დავალება 3
+#Debugging ნიშნავს error-ების ანუ შეცდომების გასწორებას
+
+#დავალება 4
+# Python-ში შეცდომების ძირითადი ტიპები:
+
+# 1. SyntaxError – კოდის სინტაქსის დარღვევა (მაგ: გამოტოვებული :, არასწორი ინდენტაცია)
+# 2. NameError – არარსებული ცვლადის ან ფუნქციის გამოყენება
+# 3. TypeError – არასწორი ტიპის მონაცემებზე ოპერაცია
+# 4. ValueError – სწორი ტიპი, მაგრამ არასწორი მნიშვნელობა
+# 5. IndexError – არარსებულ ინდექსზე წვდომა (list, tuple)
+# 6. KeyError – არარსებული key dictionary-ში
+# 7. ZeroDivisionError – ნულზე გაყოფა
+# 8. AttributeError – ობიექტს არ აქვს მოთხოვნილი ატრიბუტი
+# 9. ImportError / ModuleNotFoundError – მოდული ვერ მოიძებნა
+
+#დავალება 5
+name = "Lika"
+print(name)
+
+number = 5
+text = "apples"
+result = "number "+ "text"
+print(result)
+
+name = "liKa"
+name2 = name + "4"
+
+first_user = "Lika"
+user2nd = "Giorgi"
+print(first_user)
+
+first_name = "Data"
+last_name = "random"
+
+first_user = "Lika"
+user2nd = "Giorgi"
+print(first_user)
+
+#დავალება 7
+city = input('Enter your city: ')
+district = input('Enter your district: ')
+
+print(city)
+print(district)
+
+#დავალება 8
+age = input("my age :")
+print(f"You are {age} years old")
+
+#დავალება 9
+Num1 = input()
+Num2 = input()
+print(Num1 + Num2)
+
+
+
+
+
+
